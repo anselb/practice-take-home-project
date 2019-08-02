94a79cb1e15e54a4b2ac1128cc7cb99bb15b7ff1
Ansel Bridgewater <iamansel@gmail.com>
2019-08-02 00:50:45 -0700

    Added inline styles for Mood data div

--- src/Mood.js
+++ src/Mood.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Mood(props) {
   return (
-    <div>
+    <div style={{ border: "1px solid black", display: "inline-block", padding: "5px", margin: "5px" }}>
       <p>Mood: {props.moodInfo.mood}</p>
       <p>Date of Mood: {props.moodInfo.date}</p>
       <p>Temperature on Day: {props.moodInfo.weather.temp}</p>
